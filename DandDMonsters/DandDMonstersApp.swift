bfb0527362aa52c912b74c9d1e07dbb252817741
Robert Beachill <bbeachill@icloud.com>
2025-05-23 22:49:35 +0100

    Initial Commit

---
+++ DandDMonsters/DandDMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandDMonstersApp.swift
+//  DandDMonsters
+//
+//  Created by Robert Beachill on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DandDMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
